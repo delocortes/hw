2b57193951fbe8a43f33e741c5ec8c657a70a789
delocortes <50380166+delocortes@users.noreply.github.com>
2019-05-13 13:26:46 -0500

    Add files via upload

---
+++ JSHW/review.js
@@ -0,0 +1,59 @@
+// print to console [console.log(value);]
+console.log('Hello World');
+
+// variables - [variable variablename = value;]
+var buddy = 'my friend';
+var lightSwitch = true;
+var books = 2;
+var sentence = 'Hello my name is Deangelo';
+console.log(buddy);
+console.log('buddy');
+console.log(lightSwitch);
+var utencil = 'pencil';
+var pen = 7;
+console.log(utencil);
+console.log(pen);
+
+
+// making an equation [var equationname = value X value;]
+var equation1 = 18 + 5;
+var equation2 = 11 - 7;
+var equation3 = 12 * 12;
+var equation4 = equation1 * 4;
+
+
+// array [var listname = [];]
+var groceryList = [
+    'Tea',
+    'Sugar',
+    'Cookies',
+    'Honey'
+];
+var list = [equation1, equation2, equation3];
+console.log(groceryList, equation1);
+console.log(equation2, equation3, equation4);
+var equation4 = equation1 * 4;
+var key = [
+    2,
+    5,
+    1
+];
+
+
+// adding to  array [listname.push(value);]
+key.push(22);
+key.push(11);
+console.log(key);
+console.log(key.splice(1, 2));
+console.log(key.splice(1));
+
+//splice [console.log(listname.splice(value));]
+var list1 = ['hello', 'this', 'is', 'list1'];
+console.log(list1.splice(2));
+console.log(groceryList);
+groceryList.reverse();
+console.log(groceryList);
+console.log(typeof lightSwitch);
+var num1 = 36;
+var ageSentence = `Your age is ${num1}`;
+console.log(ageSentence);
